aadd0900a6e3651047015c299baf17d083f16609
Freddy Guen <freddy_guenengafo@yahoo.fr>
2022-02-23 18:42:59 +0100

    Traitement du filtre des projets

--- public/assets/portfolio/js/script.js
+++ public/assets/portfolio/js/script.js
@@ -68,3 +68,39 @@ function navHighlighter()
         }
     })
 }
+
+/*=============== PROJECT ITEM FILTER ===============*/
+const filterContainer = document.querySelector(".projets-filtre-interne"),
+    filterBtns = filterContainer.children;
+totalFilterBtn = filterBtns.length;
+portfolioItems = document.querySelectorAll(".projets-item");
+totalPortfolioItem = portfolioItems.length;
+
+for(let i=0; i<totalFilterBtn; i++)
+{
+    filterBtns[i].addEventListener("click", function()
+    {
+        filterContainer.querySelector(".active").classList.remove("active");
+        this.classList.add("active");
+
+        const filterValue = this.getAttribute("data-filter");
+        for(let k=0; k<totalPortfolioItem; k++)
+        {
+            if(filterValue === portfolioItems[k].getAttribute("data-category"))
+            {
+                portfolioItems[k].classList.remove("hide");
+                portfolioItems[k].classList.add("show");
+            }
+            else
+            {
+                portfolioItems[k].classList.add("hide");
+                portfolioItems[k].classList.remove("show");
+            }
+            if(filterValue === "all")
+            {
+                portfolioItems[k].classList.remove("hide");
+                portfolioItems[k].classList.add("show");
+            }
+        }
+    })
+}
